fab801275e3884b27c2010cff27644db8aa9c9ee
wedocc2026 <wedocc2026@users.noreply.github.com>
2026-06-17 08:41:26 +0000

    fix(communication): corrigir index.ts com \n literais e paths sem aspas

---
+++ plataforma-lia/src/components/communication/index.ts
@@ -0,0 +1,4 @@
+export { MessageComposer } from './message-composer'
+export type { MessageComposerProps, MessageChannel, ToneStyle } from './message-composer'
+export { ScheduleMessageModal } from './ScheduleMessageModal'
+export type { ScheduleMessageModalProps } from './ScheduleMessageModal'
